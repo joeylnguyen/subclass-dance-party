dcaf563bb1efcde3b5b9ab38c0094ff79d7311cc
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 19:06:36 -0700

    Get dancers to appear on dance floor

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,7 +4,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  var oldStep = blinkyDancer.step; // = dancer.step
 
   blinkyDancer.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
@@ -17,3 +17,23 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   return blinkyDancer;
 };
+
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+//   // this = Object.create(makeBlinkyDancer.prototype)
+//   makeDancer.call(this, top, left, timeBetweenSteps);
+//   // Store the inherited step method
+//   this.oldStep =
+// };
+// // Set prototype
+// makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+// // Set constructor
+// makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+// // Create an old step method that uses the inherited step method
+//   // var oldStep = this.step;
+// // Create step method that uses the inherited step method
+// makeBlinkyDancer.prototype.step = function() {
+//   // Call the inherited step function
+//   this.step();
+//   // Then call toggle
+//   this.$node.toggle();
+// };
--- src/dancer.js
+++ src/dancer.js
@@ -4,7 +4,7 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   var dancer = {};
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  dancer.$node = $('<span class="dancer" class="addDancerButton"></span>');
 
   dancer.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
@@ -30,3 +30,28 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+
+// var makeDancer = function(top, left, timeBetweenSteps) {
+//   this.dancer = {};
+//   // use jQuery to create an HTML <span> tag
+//   this.$node = $('<span class="dancer"></span>');
+//   this.step();
+//   this.setPosition(top, left);
+// };
+// // Add methods to makeDancer prototype
+// makeDancer.prototype.step = function() {
+//   // the basic dancer doesn't do anything interesting at all on each step,
+//   // it just schedules the next step
+//   setTimeout(this.step, timeBetweenSteps);
+// };
+
+// makeDancer.prototype.setPosition = function(top, left) {
+//   // Use css top and left properties to position our <span> tag
+//   // where it belongs on the page. See http://api.jquery.com/css/
+//   //
+//   this.styleSettings = {
+//     top: top,
+//     left: left
+//   };
+//   this.$node.css(styleSettings);
+// };
